71a0ed420423adfeddf256d4c30c7c8d09bf5228
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-10-12 12:42:26 +0300

    Update bloc_observer.dart

--- lib/core/bloc_observer.dart
+++ lib/core/bloc_observer.dart
@@ -1,28 +1,28 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '/../core/functions/debug_print_extension.dart';
 
 class MyBlocObserver extends BlocObserver {
   @override
   void onCreate(BlocBase bloc) {
     super.onCreate(bloc);
-    debugPrint('onCreate -- ${bloc.runtimeType}');
+    'onCreate -- ${bloc.runtimeType}'.dPrint();
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    debugPrint('onChange -- ${bloc.runtimeType}, $change');
+    'onChange -- ${bloc.runtimeType}, $change'.dPrint();
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    debugPrint('onError -- ${bloc.runtimeType}, $error');
+    'onError -- ${bloc.runtimeType}, $error'.dPrint();
     super.onError(bloc, error, stackTrace);
   }
 
   @override
   void onClose(BlocBase bloc) {
     super.onClose(bloc);
-    debugPrint('onClose -- ${bloc.runtimeType}');
+    'onClose -- ${bloc.runtimeType}'.dPrint();
   }
 }
