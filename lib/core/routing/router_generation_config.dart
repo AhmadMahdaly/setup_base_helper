77859b67cfae64aad1e401b1fe6c3cb31fd1ca54
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-10-12 13:08:13 +0300

    Update router_generation_config.dart

--- lib/core/routing/router_generation_config.dart
+++ lib/core/routing/router_generation_config.dart
@@ -1,11 +1,13 @@
 // import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'package:.../core/di.dart';
 import 'package:go_router/go_router.dart';
-import '../routing/app_routes.dart';
+import '/../core/constants.dart';
+import '/../core/routing/app_routes.dart';
 // import '../views/splash_view.dart';
 
 class RouterGenerationConfig {
   static GoRouter goRouter = GoRouter(
+    navigatorKey: navigatorKey,
     initialLocation: AppRoutes.splashScreen,
     routes: [
       GoRoute(
@@ -13,19 +15,28 @@ class RouterGenerationConfig {
         name: AppRoutes.splashScreen,
         // builder: (context, state) => const SplashView(),
       ),
-     // GoRoute(
-     // path: AppRoutes.editTransactionScreen,
-     // name: AppRoutes.editTransactionScreen,
-     // builder: (context, state) {
-     //   final transaction = state.extra! as Transaction;
-     //   return BlocProvider.value(
-     //     value: sl<TransactionCubit>(),
-     //     child: EditTransactionScreen(
-     //       transaction: transaction,
-     //     ),
-     //   );
-     // },
-     //),
+
+      /// ------------- < >  -------------
+      // GoRoute(
+      //     path: AppRoutes.allCoursesScreen,
+      //     name: AppRoutes.allCoursesScreen,
+      //     builder: (context, state) {
+      //       final args = state.extra! as Map<String, dynamic>;
+      //       final title = args['title'] as String;
+      //       final subject = args['items'] as List<Course>;
+      //       final subscriptions = args['subscriptions'] as List<Subscription>;
+      //       final currencySymbol = args['currencySymbol'] as String?;
+      //       return BlocProvider<CartBloc>.value(
+      //         value: getIt<CartBloc>(),
+      //         child: AllContentScreen(
+      //           items: subject,
+      //           title: title,
+      //           subscriptions: subscriptions,
+      //           currencySymbol: currencySymbol,
+      //         ),
+      //       );
+      //     },
+      //   ),
     ],
   );
 }
